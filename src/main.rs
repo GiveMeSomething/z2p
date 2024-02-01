3f5f97f58a7527ca3a2f269d031e7b404948e716
Hoang Tien Minh <htminh.1001@gmail.com>
2024-02-01 18:06:35 +0700

    feat: add simple greet endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(request: HttpRequest) -> impl Responder {
+    let name = request.match_info().get("name").unwrap_or("World!");
+
+    HttpResponse::Ok().body(format!("Hello {}", name))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
