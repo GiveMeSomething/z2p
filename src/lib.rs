fc6488368f1c88ba6b4a9b804d062df00237cb88
Hoang Tien Minh <htminh.1001@gmail.com>
2024-02-02 10:24:18 +0700

    feat: add health_check endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
+use std::io::Result;
+
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
+pub async fn run() -> std::io::Result<()> {
     HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .bind("localhost:8000")?
         .run()
         .await
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    run().await
 }
