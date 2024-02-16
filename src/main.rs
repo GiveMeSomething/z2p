1fbb8018193ad814c471f2575e98ca291d96f3d7
Hoang Tien Minh <htminh.1001@gmail.com>
2024-02-16 16:33:14 +0700

    feat: add configuration file and logic to read from configuration file

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,22 @@ async fn main() -> std::io::Result<()> {
     let configurations =
         configurations::read_configuration().expect("Failed to read configurations.");
 
+    let db_config = configurations.database;
+    println!(
+        "Starting server with database {}",
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            db_config.username,
+            db_config.password,
+            db_config.host,
+            db_config.port,
+            db_config.database_name
+        )
+    );
+
     let app_address = format!("localhost:{}", configurations.app_port);
+    println!("Starting server at http://{}", app_address);
+
     let listener =
         TcpListener::bind(app_address).expect("Failed to create TCP listener on port 8000");
 
