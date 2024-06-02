a2b2a71c8dbb0fd76ea27c80cdadb6cb82f5d7de
Hoang Tien Minh <htminh.1001@gmail.com>
2024-06-02 18:15:39 +0700

    feat: add SubscriberEmail type skeleton and add unit test

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -1,1 +1,45 @@
+#[derive(Debug)]
+pub struct SubscriberEmail(String);
 
+impl SubscriberEmail {
+    pub fn parse(s: String) -> Result<Self, String> {
+        Ok(Self(s))
+    }
+}
+
+impl AsRef<str> for SubscriberEmail {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use claims::assert_err;
+
+    use crate::domain::subscriber_email::SubscriberEmail;
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn missing_at_symbol_is_rejected() {
+        let email = "helloworld".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn missing_subject_is_rejected() {
+        let email = "@helloworld".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn missing_domain_is_rejected() {
+        let email = "helloworld@".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+}
