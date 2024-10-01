6eb08c7f46fba75f5063e53e820402235fc41f15
Hoang Tien Minh <htminh.1001@gmail.com>
2024-10-01 22:57:49 +0700

    feat: add test for email client with mock server and fake

--- src/email_client.rs
+++ src/email_client.rs
@@ -22,7 +22,7 @@ impl EmailClient {
         recipient: SubscriberEmail,
         subject: &str,
         html_content: &str,
-        text_contet: &str,
+        text_content: &str,
     ) -> Result<(), String> {
         todo!()
     }
@@ -30,8 +30,44 @@ impl EmailClient {
 
 #[cfg(test)]
 mod tests {
+    use fake::{
+        faker::{
+            internet::en::SafeEmail,
+            lorem::en::{Paragraph, Sentence},
+        },
+        Fake,
+    };
+    use wiremock::{matchers::any, Mock, MockServer, ResponseTemplate};
+
+    use crate::domain::subscriber_email::SubscriberEmail;
+    use crate::email_client::EmailClient;
+
     #[tokio::test]
     async fn send_email_send_request() {
-        todo!()
+        // Create a new HTTP server with wiremock
+        let mock_server = MockServer::start().await;
+
+        // Mock email client with new email sender
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender);
+
+        // Setup mock server
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        // Setup email received and email content
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..2).fake();
+
+        // Act
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        // Assert
     }
 }
