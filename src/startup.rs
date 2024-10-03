2babd6e16e5312be1d6e017cf84fb6026f51815a
Hoang Tien Minh <htminh.1001@gmail.com>
2024-10-03 23:58:14 +0700

    refactor: extract startup data into a new type

--- src/startup.rs
+++ src/startup.rs
@@ -5,10 +5,12 @@ use actix_web::{
     dev::{Server, Service, ServiceResponse},
     test, web, App, Error, HttpServer,
 };
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
 
 use crate::{
+    configurations::Settings,
     email_client::EmailClient,
     routes::{health_check, subscribe},
 };
@@ -29,6 +31,63 @@ use crate::{
  */
 pub async fn spawn_app() -> impl Service<Request, Response = ServiceResponse, Error = Error> {
     test::init_service(App::new().route("health_check", web::get().to(health_check))).await
+}
+
+pub fn build_email_client(config: &Settings) -> EmailClient {
+    let sender = config
+        .email_client
+        .sender()
+        .expect("Invalid sender's email");
+    let timeout = config.email_client.timeout;
+    EmailClient::new(
+        config.email_client.base_url.to_owned(),
+        sender,
+        config.email_client.auth_token.to_owned(),
+        std::time::Duration::from_millis(timeout),
+    )
+}
+
+pub fn build_connection_pool(config: &Settings) -> PgPool {
+    PgPool::connect_lazy(config.database.connection_string().expose_secret()).unwrap_or_else(
+        |err| {
+            panic!(
+                "Failed to connect to the datbase connection pool with error {:?}",
+                err
+            )
+        },
+    )
+}
+
+struct Application {
+    port: u16,
+    server: Server,
+}
+
+impl Application {
+    pub async fn build(config: Settings) -> Result<Self, std::io::Error> {
+        let db_pool = build_connection_pool(&config);
+        let email_client = build_email_client(&config);
+
+        let address = format!(
+            "{}:{}",
+            config.application.host.to_owned(),
+            config.application.port.to_owned()
+        );
+        let listener = TcpListener::bind(address)?;
+        let port = listener.local_addr().unwrap().port();
+        let server = run(listener, db_pool, email_client).await?;
+
+        Ok(Self { port, server })
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    // Consume self
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
 }
 
 pub async fn run(
