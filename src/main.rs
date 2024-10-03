d7f2795cd58467853e39e86bd1bd5d37872f5b8b
Hoang Tien Minh <htminh.1001@gmail.com>
2024-10-03 23:58:14 +0700

    refactor: extract startup data into a new type

--- src/main.rs
+++ src/main.rs
@@ -13,32 +13,4 @@ async fn main() -> std::io::Result<()> {
 
     let configurations =
         configurations::read_configuration().expect("Failed to read configurations.");
-
-    // Setup database connection pool
-    let db_connection_pool = configurations.database.pg_connection_pool();
-
-    // Setup email client (also a connection pool underneath)
-    let email_sender = configurations
-        .email_client
-        .sender()
-        .expect("Invalid sender's email address");
-    let email_client = EmailClient::new(
-        configurations.email_client.base_url,
-        email_sender,
-        configurations.email_client.auth_token,
-        std::time::Duration::from_micros(configurations.email_client.timeout),
-    );
-
-    let app_address = format!(
-        "{}:{}",
-        configurations.application.host, configurations.application.port
-    );
-    println!("Starting server at http://{}", app_address);
-
-    let listener =
-        TcpListener::bind(app_address).expect("Failed to create TCP listener on port 8000");
-    run(listener, db_connection_pool, email_client)
-        .await
-        .unwrap()
-        .await
 }
