3ced3853fed11240773b1756b4c6f70d3c9ce4fb
Hoang Tien Minh <htminh.1001@gmail.com>
2024-10-04 00:50:11 +0700

    fix: fix linting errors

--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,6 @@
 use std::net::TcpListener;
 
-use actix_http::Request;
-use actix_web::{
-    dev::{Server, Service, ServiceResponse},
-    test, web, App, Error, HttpServer,
-};
+use actix_web::{dev::Server, web, App, HttpServer};
 use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
@@ -14,24 +10,6 @@ use crate::{
     email_client::EmailClient,
     routes::{health_check, subscribe},
 };
-
-/**
- * Actix provide some conveniences to interact with an App without skipping the routing logic
- *
- * But,
- *
- * 1. Migrating to another web framework would force us to rewrite our whole integration test suite.
- *    And we'd like our integration tests ot be highly decoupled
- *
- * 2. Due to some limitations, we cannot share the App startup logic between our production and test logic
- * => Risk of divergence over time
- *
- * Therefore, we will opt for a fully black-box solution,
- * we will launch our application at the beginning of each tests and interact with it using a HTTP Client
- */
-pub async fn spawn_app() -> impl Service<Request, Response = ServiceResponse, Error = Error> {
-    test::init_service(App::new().route("health_check", web::get().to(health_check))).await
-}
 
 pub fn build_email_client(config: &Settings) -> EmailClient {
     let sender = config
