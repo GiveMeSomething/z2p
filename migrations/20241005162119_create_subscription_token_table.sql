c684b8ce2594b390580b933f764834b23464d7c1
Hoang Tien Minh <htminh.1001@gmail.com>
2024-10-05 23:25:48 +0700

    chore: migrate database with new migrations

---
+++ migrations/20241005162119_create_subscription_token_table.sql
@@ -0,0 +1,8 @@
+-- Add migration script here
+CREATE TABLE subscription_tokens(
+  subscription_token TEXT NOT NULL,
+  subscriber_id UUID NOT NULL,
+
+  PRIMARY KEY (subscription_token),
+  FOREIGN KEY (subscriber_id) REFERENCES subscriptions (id)
+);
